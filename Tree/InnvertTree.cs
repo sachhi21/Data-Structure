95643f522c8f0a43a512762173825fd84741e785
Sachin Rampur <99388531+sachhi21@users.noreply.github.com>
2025-02-09 00:57:16 +0530

    Add files via upload

---
+++ Tree/InnvertTree.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree
+{
+    public class InnvertTree
+    {
+
+        public BNode InvertTree(BNode root)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+            BNode rl = root.Left;
+            root.Left = root.Right;
+            root.Right = rl;
+
+             InvertTree(root.Left);
+             InvertTree(root.Right);
+            return root;
+        }   
+    }
+}
---
+++ Tree/RightSideViewTree.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree
+{
+    public class RightSideViewTree
+    {
+
+
+        public IList<int> RightSideView(BNode root)
+        {
+            List<int> result = new List<int>();
+            RightSideViewHelper(root, 0, result);
+            return result;
+        }
+
+        private void RightSideViewHelper(BNode node, int level, List<int> result)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+
+            if (level == result.Count)
+            {
+                result.Add(node.Data);
+            }
+
+            RightSideViewHelper(node.Right, level + 1, result);
+            RightSideViewHelper(node.Left, level + 1, result);
+        }
+    }
+}
