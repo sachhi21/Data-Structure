c7fa6f7aa2e05e211b34173575e9406d9ffa88e2
Sachin Rampur <99388531+sachhi21@users.noreply.github.com>
2025-02-09 00:43:57 +0530

    Add files via upload

---
+++ Tree/CountMinimumNodeLeaf.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree
+{
+    public class CountMinimumNodeLeaf
+    {
+        public int CountNodes(BNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            if (root.Left == null)
+            {
+                return 1 + CountNodes(root.Right);
+            }
+
+
+            if (root.Right == null)
+            {
+                return 1 + CountNodes(root.Left);
+            }
+
+            return 1 + Math.Min(CountNodes(root.Left), CountNodes(root.Right));
+
+        }
+    }
+}
---
+++ Tree/IsSameTrees.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree
+{
+    public class IsSameTrees
+    {
+        bool result = true;
+        public bool IsSameTree(BNode p, BNode q)
+        {
+            if (p == null && q == null)
+            {
+                return true;
+            }
+
+            if (p == null || q == null)
+            {
+                return false;
+            }
+
+            if (p.Data != q.Data)
+            {
+                return false;
+            }
+
+           return  IsSameTree(p.Left, q.Left) && IsSameTree(p.Right, q.Right);
+
+
+
+
+        }
+    }
+}
